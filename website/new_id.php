5f5d873a8bb8f3cb04ff9fd51abe371f61ddee98
Johndave110 <johndavelibradilla@gmail.com>
2025-12-07 15:50:12 +0800

    update

--- website/new_id.php
+++ website/new_id.php
@@ -132,7 +132,7 @@
                 
                 <div class="flex flex-col md:flex-row justify-between md:items-center mb-8 gap-4">
                     <h2 class="text-xl font-bold brand-blue-text">Personal Na Impormasyon</h2>
-                    <button class="border border-gray-300 text-gray-500 text-sm px-6 py-2 rounded-md bg-white hover:bg-gray-50 w-full md:w-auto text-center">Input Name</button>
+                    <input class="border border-gray-300 text-gray-500 text-sm px-6 py-2 rounded-md bg-white hover:bg-gray-50 w-full md:w-auto text-center" placeholder="Input Employee Name">
                 </div>
 
                 <form>
